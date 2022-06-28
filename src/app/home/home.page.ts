82d02f505110530a027589ff7824beb8cb0989d5
studentrk <studentrk@outlook.it>
2022-06-28 21:23:59 +0200

    svg background sizing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,13 @@ export class HomePage {
 
   constructor() {}
 
+  addSvg() {
+    console.log("test");
+    
+    var tag = document.createElement("ion-icon");
+    tag.src = "./assets/shapes.svg"
+    tag.className = "shapesSvg"
+    var element = document.getElementById("svgContainer")
+    element.appendChild(tag);
+  }
 }
